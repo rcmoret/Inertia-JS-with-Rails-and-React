29c35ef2fa27741458281f4d7b0b65e7714ee8e5
Ryan's iMac <ryancmoret@gmail.com>
2021-09-21 15:16:27 -0500

    Set up demo env

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,14 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, unless: :demo_env?
   after_action :add_csrf_token
 
   private
 
   def add_csrf_token
     cookies["XSRF-TOKEN"] = form_authenticity_token
+  end
+
+  def demo_env?
+    Rails.env == 'demo'
   end
 end
