15d5082ed030a5149f8d94de5d122f0a3bdbd1fe
Ryan's iMac <ryancmoret@gmail.com>
2021-10-03 18:12:21 -0500

    Updates to money formatter

--- app/javascript/lib/MoneyFormatter.js
+++ app/javascript/lib/MoneyFormatter.js
@@ -1,15 +1,15 @@
-const MoneyFormatter = (number, opts = { absolute: false, toFloat: false, plainText: true }) => {
+const MoneyFormatter = (number, opts = { absolute: false, toFloat: false, decorate: false }) => {
   if (number === null || number === '') {
     return ""
   } else if (opts.toFloat) {
     const num = opts.absolute ? Math.abs(number) : number
     return parseFloat(num / 100.0).toFixed(2)
-  } else if (opts.plainText) {
-    const num = opts.absolute ? Math.abs(number) : number
-    return (num / 100.0).toFixed(2)
-  } else {
+  } else if (opts.decorate) {
     const num = opts.absolute ? Math.abs(number) : number
     return "$" + (num / 100.0).toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")
+  } else {
+    const num = opts.absolute ? Math.abs(number) : number
+    return (num / 100.0).toFixed(2)
   }
 }
 
@@ -18,7 +18,9 @@ export const decimalToInt = (amount) => {
   const dollars = stringArray[0]
   const cents = stringArray[1]
 
-  if (cents.length == 0) {
+  if (cents.length == 0 && dollars === '-') {
+    return 0
+  } else if (cents.length == 0) {
     return parseInt(amount * 100)
   } else if (cents.length == 2) {
     return parseInt(`${dollars}${cents}`)
