209e9fe43d7873cc863acc10e994d01819cdc8be
Ryan's iMac <ryancmoret@gmail.com>
2022-01-24 12:52:31 -0600

    Bump rails gem version to 7.0

--- config/application.rb
+++ config/application.rb
@@ -12,12 +12,13 @@ module InertiaBudget
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.web_console.development_only = false
-    config.time_zone = 'Central Time (US & Canada)'
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
