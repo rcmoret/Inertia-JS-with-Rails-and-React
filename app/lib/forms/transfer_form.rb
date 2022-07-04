fec5e109e3cb6fd4aa1425bb0f210223fc339a06
Ryan's Mac Mini <ryancmoret@gmail.com>
2022-07-04 18:16:52 -0500

    Move transfer creation to its own form

---
+++ app/lib/forms/transfer_form.rb
@@ -0,0 +1,83 @@
+# frozen_string_literal: true
+
+module Forms
+  class TransferForm
+    include ActiveModel::Model
+
+    validates :to_account, :from_account, presence: true
+    validate :separate_accounts
+    validates :amount, numericality: { greater_than: 0 }
+
+    def initialize(to_account_id:, from_account_id:, amount:)
+      @to_account_id = to_account_id
+      @from_account_id = from_account_id
+      @amount = amount.to_i.abs
+    end
+
+    def call
+      return [:error, errors.to_hash] if invalid?
+
+      create!
+
+      return [:error, errors.to_hash] if errors.any?
+
+      [:ok, { transfer: transfer }]
+    end
+
+    private
+
+    def create!
+      Transfer.transaction do
+        { from_transaction: from_transaction, to_transaction: to_transaction, transfer: transfer }.each do |key, model|
+          next if model.save
+
+          promote_errors(key, model.errors)
+        end
+
+        raise ActiveRecord::Rollback if errors.any?
+      end
+    end
+
+    def transfer
+      @transfer ||= Transfer.new
+    end
+
+    def from_transaction
+      transfer.build_from_transaction(
+        description: "Transfer to #{to_account}",
+        account: from_account,
+        details_attributes: [{ amount: -amount }]
+      )
+    end
+
+    def to_transaction
+      transfer.build_to_transaction(
+        description: "Transfer from #{from_account}",
+        account: to_account,
+        details_attributes: [{ amount: amount }]
+      )
+    end
+
+    attr_reader :to_account_id, :from_account_id, :amount
+
+    def to_account
+      @to_account ||= Account.find_by(id: to_account_id)
+    end
+
+    def from_account
+      @from_account ||= Account.find_by(id: from_account_id)
+    end
+
+    def promote_errors(model_name, model_errors)
+      model_errors.each do |attribute, message|
+        errors.add("#{model_name}.#{attribute}", message)
+      end
+    end
+
+    def separate_accounts
+      return unless to_account_id == from_account_id
+
+      errors.add(:from_account, "cannot be the same account as 'to' account")
+    end
+  end
+end
---
+++ spec/lib/forms/transfer_form_spec.rb
@@ -0,0 +1,88 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Forms::TransferForm do
+  subject do
+    described_class.new(
+      to_account_id: to_account.id,
+      from_account_id: from_account.id,
+      amount: amount
+    )
+  end
+
+  describe 'validations' do
+    context 'when the "to" account is not present' do
+      let(:to_account) { FactoryBot.create(:account) }
+      let(:from_account) { FactoryBot.create(:account) }
+      let(:amount) { rand(100..1000) }
+
+      before do
+        allow(Account).to receive(:find_by).with(id: to_account.id).and_return(nil)
+        allow(Account).to receive(:find_by).with(id: from_account.id).and_call_original
+      end
+
+      it 'returns an error tuple' do
+        expect(subject.call).to match([:error, hash_including(to_account: ["can't be blank"])])
+      end
+    end
+
+    context 'when the "from" account is not present' do
+      let(:to_account) { FactoryBot.create(:account) }
+      let(:from_account) { FactoryBot.create(:account) }
+      let(:amount) { rand(100..1000) }
+
+      before do
+        allow(Account).to receive(:find_by).with(id: to_account.id).and_call_original
+        allow(Account).to receive(:find_by).with(id: from_account.id).and_return(nil)
+      end
+
+      it 'returns an error tuple' do
+        expect(subject.call).to match([:error, hash_including(from_account: ["can't be blank"])])
+      end
+    end
+
+    context 'when the both account ids are the same' do
+      let(:to_account) { FactoryBot.create(:account) }
+      let(:from_account) { to_account }
+      let(:amount) { rand(100..1000) }
+
+      it 'returns an error tuple' do
+        expect(subject.call)
+          .to eq([:error, { from_account: ["cannot be the same account as 'to' account"] }])
+      end
+    end
+
+    context 'when the amount is zero' do
+      let(:to_account) { FactoryBot.create(:account) }
+      let(:from_account) { FactoryBot.create(:account) }
+      let(:amount) { 0 }
+
+      it 'returns an error tuple' do
+        expect(subject.call).to match([:error, hash_including(amount: ['must be greater than 0'])])
+      end
+    end
+
+    context 'when the amount is a negative number' do
+      let(:to_account) { FactoryBot.create(:account) }
+      let(:from_account) { FactoryBot.create(:account) }
+      let(:amount) { -1 * rand(100..1000) }
+
+      it 'returns an :ok tuple' do
+        expect(subject.call).to match([:ok, anything])
+      end
+
+      it 'creates a new transaction entry for the to account' do
+        expect { subject.call }.to change { Transaction::Entry.where(account_id: to_account.id).count }.by(+1)
+      end
+
+      it 'creates a new transaction entry for the to account' do
+        expect { subject.call }.to change { Transaction::Entry.where(account_id: from_account.id).count }.by(+1)
+      end
+
+      it 'creates a new transfer' do
+        expect { subject.call }.to change { Transfer.count }.by(+1)
+      end
+    end
+  end
+end
