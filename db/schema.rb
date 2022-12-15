8b18f86965e6b708959c99d27796483dc3a61d21
Ryan's Mac Mini <ryancmoret@gmail.com>
2022-12-15 11:41:58 -0600

    Add key to transaction detail

--- db/schema.rb
+++ db/schema.rb
@@ -19,9 +19,9 @@ ActiveRecord::Schema.define(version: 2023_01_15_163623) do
     t.string "name", null: false
     t.boolean "cash_flow", default: true
     t.integer "priority", null: false
-    t.datetime "archived_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "archived_at", precision: 6
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.string "slug", limit: 30
     t.integer "user_id"
   end
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 2023_01_15_163623) do
     t.string "record_type", null: false
     t.bigint "record_id", null: false
     t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
+    t.datetime "created_at", precision: 6, null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 2023_01_15_163623) do
     t.text "metadata"
     t.bigint "byte_size", null: false
     t.string "checksum"
-    t.datetime "created_at", null: false
+    t.datetime "created_at", precision: 6, null: false
     t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
@@ -60,10 +60,10 @@ ActiveRecord::Schema.define(version: 2023_01_15_163623) do
     t.boolean "monthly", default: true, null: false
     t.boolean "expense", default: true, null: false
     t.boolean "accrual", default: false, null: false
-    t.datetime "archived_at"
+    t.datetime "archived_at", precision: 6
     t.bigint "icon_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.string "slug"
     t.boolean "is_per_diem_enabled", default: false, null: false
     t.bigint "user_id"
@@ -81,12 +81,12 @@ ActiveRecord::Schema.define(version: 2023_01_15_163623) do
   create_table "budget_intervals", force: :cascade do |t|
     t.integer "month", null: false
     t.integer "year", null: false
-    t.datetime "set_up_completed_at"
-    t.datetime "close_out_completed_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.datetime "start_date"
-    t.datetime "end_date"
+    t.datetime "set_up_completed_at", precision: 6
+    t.datetime "close_out_completed_at", precision: 6
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.date "start_date"
+    t.date "end_date"
     t.bigint "user_id", null: false
     t.index ["month", "year", "user_id"], name: "index_budget_intervals_on_month_and_year_and_user_id", unique: true
     t.index ["user_id"], name: "index_budget_intervals_on_user_id"
@@ -115,9 +115,9 @@ ActiveRecord::Schema.define(version: 2023_01_15_163623) do
   create_table "budget_items", force: :cascade do |t|
     t.bigint "budget_category_id", null: false
     t.bigint "budget_interval_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.datetime "deleted_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "deleted_at", precision: 6
     t.string "key", limit: 12, null: false
     t.index ["budget_category_id"], name: "index_budget_items_on_budget_category_id"
     t.index ["budget_interval_id"], name: "index_budget_items_on_budget_interval_id"
@@ -127,8 +127,8 @@ ActiveRecord::Schema.define(version: 2023_01_15_163623) do
   create_table "icons", force: :cascade do |t|
     t.string "name", limit: 100, null: false
     t.string "class_name", limit: 100, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.index ["class_name"], name: "index_icons_on_class_name", unique: true
     t.index ["name"], name: "index_icons_on_name", unique: true
   end
@@ -137,8 +137,8 @@ ActiveRecord::Schema.define(version: 2023_01_15_163623) do
     t.bigint "transaction_entry_id", null: false
     t.bigint "budget_item_id"
     t.integer "amount", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.string "key", limit: 12
     t.index ["budget_item_id"], name: "index_transaction_details_on_budget_item_id"
     t.index ["key"], name: "index_transaction_details_on_key", unique: true
@@ -153,26 +153,25 @@ ActiveRecord::Schema.define(version: 2023_01_15_163623) do
     t.text "notes"
     t.boolean "budget_exclusion", default: false, null: false
     t.bigint "transfer_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "key", limit: 12
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.index ["account_id"], name: "index_transaction_entries_on_account_id"
     t.index ["transfer_id"], name: "index_transaction_entries_on_transfer_id"
   end
 
   create_table "transfers", force: :cascade do |t|
-    t.integer "to_transaction_id"
-    t.integer "from_transaction_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer "to_transaction_id", null: false
+    t.integer "from_transaction_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at", precision: 6
+    t.datetime "remember_created_at", precision: 6
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
