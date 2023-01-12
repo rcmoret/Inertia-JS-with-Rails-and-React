94419018e89cfd0e35736144fa87fbc99ce01023
Ryan's Mac Mini <ryancmoret@gmail.com>
2023-01-12 10:47:23 -0600

    Adjust account presenter

--- app/lib/presenters/account_presenter.rb
+++ app/lib/presenters/account_presenter.rb
@@ -17,10 +17,10 @@ module Presenters
         .map(&:as_presenter)
     end
 
-    def cash_flow?
+    def cash_flow
       super
     end
-    alias is_cash_flow cash_flow?
+    alias is_cash_flow cash_flow
 
     def archived?
       archived_at.present?
