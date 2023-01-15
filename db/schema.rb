ceb2cf8e76f732738fc36ac3926d9eff322b5c4c
Ryan's Mac Mini <ryancmoret@gmail.com>
2023-01-15 10:40:20 -0600

    Add key column to transaction entry

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_03_234610) do
+ActiveRecord::Schema.define(version: 2023_01_15_163623) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -155,6 +155,7 @@ ActiveRecord::Schema.define(version: 2023_01_03_234610) do
     t.bigint "transfer_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "key", limit: 12
     t.index ["account_id"], name: "index_transaction_entries_on_account_id"
     t.index ["transfer_id"], name: "index_transaction_entries_on_transfer_id"
   end
