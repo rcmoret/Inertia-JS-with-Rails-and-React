8dfdff4cd3dfbfbe13c953bc98fb1abc9b47b207
Ryan's Mac Mini <ryancmoret@gmail.com>
2023-02-22 14:01:12 -0600

    Change webpack config for production

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,15 +1,7 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 const environment = require('./environment')
 
-environment.plugins.delete('UglifyJs')
 
-environment.plugins.append(
-  'UglifyJs',
-  new UglifyJsPlugin({
-    sourceMap: true
-  })
-)
 
 module.exports = environment.toWebpackConfig()
