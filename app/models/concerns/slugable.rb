383c4208541ce1fe1eea9c7fe66c251645933f77
Ryan's Mac Mini <ryancmoret@gmail.com>
2023-02-26 16:10:15 -0600

    Update slug regexp

--- app/models/concerns/slugable.rb
+++ app/models/concerns/slugable.rb
@@ -4,10 +4,11 @@ module Slugable
   extend ActiveSupport::Concern
 
   SLUG_FORMAT_MESSAGE = 'must be combination of lowercase letters, numbers and dashes'
+  SLUG_REGEXP = /\A[a-z][a-z0-9-]*[a-z0-9]\z/.freeze
 
   included do
     validates :slug, presence: true
-    validates :slug, format: { with: /\A[a-z0-9-]+\Z/, message: SLUG_FORMAT_MESSAGE }
+    validates :slug, format: { with: SLUG_REGEXP, message: SLUG_FORMAT_MESSAGE }
     validates :slug, uniqueness: { scope: :user_group_id }
   end
 
