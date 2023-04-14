8199f117bfcbf83f7c5acbea2f9445a3f69d21e6
Ryan's Mac Mini <ryancmoret@gmail.com>
2023-04-14 15:00:53 -0500

    Update user group info

--- app/lib/queries/transactions/discretionary_details_query.rb
+++ app/lib/queries/transactions/discretionary_details_query.rb
@@ -41,7 +41,7 @@ module Queries
         base_clause = TRANSACTION_DETAILS[:budget_item_id].eq(nil)
         base_clause = base_clause.and(TRANSACTIONS[:budget_exclusion].eq(false))
         base_clause = base_clause.and(TRANSFERS[:id].eq(nil))
-        base_clause = base_clause.and(ACCOUNTS[:user_group_id]).eq(user_group.id)
+        base_clause = base_clause.and(ACCOUNTS[:user_group_id]).eq(interval.user_group_id)
 
         date_clause = TRANSACTIONS[:clearance_date].between(date_range)
         date_clause = date_clause.or(TRANSACTIONS[:clearance_date].eq(nil)) if current?
